ee1fab3af98f855be08bcc1e4984d327e508aaca
jasurrahmon24 <jasurrahmon24@gmail.com>
2022-10-07 19:37:26 +0500

    Hibernate: save objects to table

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -21,9 +21,12 @@ public class App
         try {
             session.beginTransaction();
 
-            Person person = session.get(Person.class, 1);
-            System.out.println(person.getName());
-            System.out.println(person.getAge());
+           Person person1 = new Person("Samir", 1);
+           Person person2 = new Person("Naima", 26);
+           Person person3 = new Person("Ulugbek", 24);
+           session.save(person1);
+           session.save(person2);
+           session.save(person3);
 
             session.getTransaction().commit();
 
--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,7 +14,14 @@ import javax.persistence.Table;
 public class Person {
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     @Column(name = "name")
     private String name;
     @Column(name = "age")
