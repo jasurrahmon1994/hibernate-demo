5bd4f3eaed96298127560799cc1546602260e595
jasurrahmon24 <jasurrahmon24@gmail.com>
2022-10-07 18:56:59 +0500

    hibernate configuration & get result from table

---
+++ src/main/java/org/example/models/Person.java
@@ -0,0 +1,25 @@
+package org.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "Person")
+public class Person {
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private int age;
+}
