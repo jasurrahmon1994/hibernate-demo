8aa739793d9c03281413fb31f75a97017822c553
jasurrahmon24 <jasurrahmon24@gmail.com>
2022-10-07 20:26:50 +0500

    HQL: create custom query

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,14 +5,13 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
@@ -20,7 +19,7 @@ public class App
 
         try {
             session.beginTransaction();
-// Updating entity in table
+//            Updating entity in table
 //            Person person = session.get(Person.class, 1);
 //            person.setAge(28);
 
@@ -28,11 +27,21 @@ public class App
             // Saving Entity to the table
 //            Person begona = new Person("Begona", 55);
 //            session.save(begona);
-            
+
             // Deleting entity from the table
 
-            Person person = session.get(Person.class, 5);
-            session.delete(person);
+//            Person person = session.get(Person.class, 5);
+//            session.delete(person);
+
+//            List<Person> list = session.createQuery("FROM Person where age > 10").getResultList();
+//            list.forEach(person -> {
+//                System.out.println(person.getName());
+//                System.out.println(person.getAge());
+//            });
+
+//            session.createQuery("update Person set name = 'Jasurbek' where name = 'Jasur'").executeUpdate();
+            session.createQuery("delete from Person where name = 'Ulugbek'").executeUpdate();
+
 
             session.getTransaction().commit();
 
